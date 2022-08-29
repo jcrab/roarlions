11457de9d2d0bddb4cce99a2ef0600b56cf9051e
jcrab <jcrabtree@una.edu>
2022-08-29 14:20:03 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Roar Lions</title>
+    </head>
+    <body>
+        <h2>Roar Lions Project</h2>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
